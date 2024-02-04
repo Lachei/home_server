b12a2cc570d78574abdcbe5b1393645d4b8a189c
Josef Stumpfegger <josefstumpfegger@outlook.de>
2024-02-04 18:52:53 +0100

    Better install script which now is able to also only handle a single command line input

--- update_server.sh
+++ update_server.sh
@@ -5,14 +5,19 @@ if [ $# -eq 0 ] ; then
     echo No arguments, installing release server in ~/apps/local_server
     install_path=~/apps/home_server
     build_type=Release
-elif [ $# -ne 2] ; then
+elif [ $# -eq 1 ] ; then
+    install_path=~/apps/home_server
+    build_type=$1
+elif [ $# -eq 2 ] ; then
+    install_path=$1
+    build_type=$2
+else 
     echo If command line parameteres are provided these always have to be
     echo    install/path                \(can be anything\)
     echo    build_type[Release/Debug]   \(only Release/Debug are allowed\)
+    echo or
+    echo    build_type[Release/Debug]   \(only Release/Debug are allowed\)
     exit 1
-else
-    install_path=$1
-    build_type=$2
 fi
 
 #copying all needed files
