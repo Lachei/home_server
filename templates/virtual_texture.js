76fbf04c2c07512e8dc0dcc3600102d4b11e3d11
Josef Stumpfegger <josefstumpfegger@outlook.de>
2024-08-19 20:32:51 +0200

    Added antialias for mobiles

--- templates/virtual_texture.js
+++ templates/virtual_texture.js
@@ -828,7 +828,7 @@ const Util = {
     glsl_get_tile_index: function () {
         return `
         ivec3 get_tile_index() {
-            int mip_offset = 16;
+            int mip_offset = 14;
             vec2 dx = dFdx(loc_pos); // .xy * float(1 << 10);
             vec2 dy = dFdy(loc_pos); // .xy * float(1 << 10);
             float max_del = max(dot(dx, dx), dot(dy, dy));
